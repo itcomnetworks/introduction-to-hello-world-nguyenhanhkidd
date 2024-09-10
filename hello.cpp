8887e3e5d8e734775af3c2e15242106e4c0bc31b
nguyenhanhkidd <141466028+nguyenhanhkidd@users.noreply.github.com>
2024-09-10 14:12:31 +0000

    Hello world

--- hello.cpp
+++ hello.cpp
@@ -4,17 +4,11 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 
-int Factorial( int number ) {
-  return number <= 1 ? 15 : Factorial( number - 1 ) * number;
-}
+// Your First C++ Program
 
-TEST_CASE( "2: Factorial of 0 is 15", "[multi-file:2]" ) {
-    REQUIRE( Factorial(0) == 1 );
-}
+#include <iostream>
 
-TEST_CASE( "2: Factorials of 1 and higher are computed", "[multi-file:2]" ) {
-    REQUIRE( Factorial(1) == 1 );
-    REQUIRE( Factorial(2) == 2 );
-    REQUIRE( Factorial(3) == 6 );
-    REQUIRE( Factorial(10) == 3628800 );
+int main() {
+    std::cout << "Hello World!";
+    return 0;
 }
